a75840abc76046cf040687116dc7f411c978702a
guillaumeBianchi <g.bianchi@hotmail.fr>
2016-10-13 18:00:29 +0200

    script bruit (in progress)

---
+++ manet/Assets/scripts/Noise.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Diagnostics;
+
+public class Noise : MonoBehaviour {
+
+	void OnTriggerEnter2D(Collider2D collision_with)
+	{
+		if (collision_with.gameObject.tag == "Player") 
+		{
+			print ("ok");
+			print (transform.parent.name);
+			print (transform.parent.position);
+		}
+	}
+}
