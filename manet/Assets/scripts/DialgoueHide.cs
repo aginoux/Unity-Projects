a02dfc52ce4f20826723f68fea2f23886d2f8e32
guillaumeBianchi <g.bianchi@hotmail.fr>
2016-10-25 16:08:43 +0200

    Ajout ombre + bulles de dialogues

---
+++ manet/Assets/scripts/DialgoueHide.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialgoueHide : MonoBehaviour {
+
+	public GameObject player;
+	public float timeLeft = 3f;
+	float temporary_timeLeft;
+
+	// Use this for initialization
+	void Start () {
+		GetComponentInChildren<SpriteRenderer>().enabled = false;
+		player = GameObject.FindGameObjectWithTag ("Player");
+		temporary_timeLeft = timeLeft;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (player.GetComponent<Player> ().in_locker == true) 
+		{
+			GetComponentInChildren<SpriteRenderer> ().enabled = true;
+			transform.position = player.transform.position;
+			timeLeft -= Time.deltaTime;
+			if(timeLeft <= 0)
+			{
+				GetComponentInChildren<SpriteRenderer>().enabled = false;
+			} 
+		}
+		else {
+			timeLeft = temporary_timeLeft;
+			GetComponentInChildren<SpriteRenderer>().enabled = false;
+		}
+	}
+}
